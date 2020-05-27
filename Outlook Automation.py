7fe53f7449443341f57bebbf05b7d416a1a54f3d
Jatin <jatinnitkkr@gmail.com>
2020-05-27 17:16:49 +0530

    Added multiple keywords list

--- Outlook Automation.py
+++ Outlook Automation.py
@@ -14,7 +14,8 @@ import openpyxl
 import datetime
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 add = input("Enter email address to Look for ")
-key = input("Enter keyword ")
+stringk = input("Enter keyword ")
+key = stringk.split(",")
 inbox = outlook.GetDefaultFolder(6)
 
 dt = str(datetime.date.today())
@@ -22,17 +23,18 @@ wb = openpyxl.Workbook()
 ws =  wb.active
 ws.title = "Sheet1"
 wb["Sheet1"]['A'+str(1)] = "Email Scrapped: " + add
-wb["Sheet1"]['B'+str(1)] = "Key Searched " + key
-wb["Sheet1"]['A'+str(2)] = 'Campaign'
-wb["Sheet1"]['B'+str(2)] = 'Docket Update'
-wb["Sheet1"]['C'+str(2)] = 'Campaign Link'
-wb["Sheet1"]['D'+str(2)] = 'Docket Link'
-wb["Sheet1"]['E'+str(2)] = 'Docket Date extracted from Docket Text'
-wb.save('C:/Users/Lumenci 3/Desktop/'+dt+key+add+'.xlsx')
+wb["Sheet1"]['B'+str(1)] = "Key Searched " + stringk
+wb["Sheet1"]['A'+str(2)] = "Keyy matched"
+wb["Sheet1"]['B'+str(2)] = 'Campaign'
+wb["Sheet1"]['C'+str(2)] = 'Docket Update'
+wb["Sheet1"]['D'+str(2)] = 'Campaign Link'
+wb["Sheet1"]['E'+str(2)] = 'Docket Link'
+wb["Sheet1"]['F'+str(2)] = 'Docket Date extracted from Docket Text'
+wb.save('C:/Users/Lumenci 3/Desktop/'+dt+stringk+add+'.xlsx')
 messages = inbox.Items
 found = ""
 length=len(messages)
-work = openpyxl.load_workbook('C:/Users/Lumenci 3/Desktop/'+dt+key+add+'.xlsx')
+work = openpyxl.load_workbook('C:/Users/Lumenci 3/Desktop/'+dt+stringk+add+'.xlsx')
 print("Yes")
 sheet = work["Sheet1"]
 k = 3
@@ -57,34 +59,34 @@ for i in range(length-1,-1,-1):
                             #lastlink = list[1]
                         #except:
                             #lastlink = "No link attached"
-                    if key in lin:
-                        found = str(lin)
+                    for keyyy in key:
+                        if keyyy in lin:
+                            found = str(lin)
+                            sheet['A'+str(k)] = keyyy
+                            print("######################")
+                            print("Campaign: ",lastcamp)
+                            print("keyword: ",found)
+                            print("Link ",lis[1].split(">")[0])
+                            sheet['B'+str(k)] = lastcamp
+                            sheet['C'+str(k)] = found
+                            try:
+                                sheet['D'+str(k)] = lis[1].split(">")[0]
+                            except:
+                                print("..")
+                            try:
+                                l1 = found.split("<")[1].split(">")[0]
+                                sheet['E'+str(k)] = l1
+                            except:
+                                print('00')
+                            try:
+                                dat = str(dparser.parse(found,fuzzy=True))
+                                sheet['F'+str(k)] = dat
+                            except:
+                                print('00')
+                            k = k+1
+                            print("######################")
                         
-                        print("dat ", dat)
-                        print("######################")
-                        print("Campaign: ",lastcamp)
-                        print("keyword: ",found)
-                        print("Link ",lis[1].split(">")[0])
-                        sheet['A'+str(k)] = lastcamp
-                        sheet['B'+str(k)] = found
-                        try:
-                            sheet['C'+str(k)] = lis[1].split(">")[0]
-                        except:
-                            print("..")
-                        try:
-                            l1 = found.split("<")[1].split(">")[0]
-                            sheet['D'+str(k)] = l1
-                        except:
-                            print('00')
-                        try:
-                            dat = str(dparser.parse(found,fuzzy=True))
-                            sheet['E'+str(k)] = dat
-                        except:
-                            print('00')
-                        k = k+1
-                        print("######################")
-                        
-                print("Over---------------------------------")
+                    print("Over---------------------------------")
                     #kk = messages[i]
                 
                     #print(colored(kk,'blue'))
@@ -95,4 +97,4 @@ for i in range(length-1,-1,-1):
             #print("www",messages[i].SenderEmailAddress)
             print(colored("Sender's Type Unknown: ",'red'),messages[i].Sender)
             #print('Class: ',messages[i].Sender)
-work.save('C:/Users/Lumenci 3/Desktop/'+dt+key+add+'.xlsx')
+work.save('C:/Users/Lumenci 3/Desktop/'+dt+stringk+add+'.xlsx')
